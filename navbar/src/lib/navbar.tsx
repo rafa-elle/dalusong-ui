f4c40229f7bb4da96d4c4f94be1ed4a2a85a9e24
rafa-elle <rafaelledalusong@gmai.com>
2025-01-30 16:59:31 +0100

    feat: init library navbar

---
+++ navbar/src/lib/navbar.tsx
@@ -0,0 +1,11 @@
+import styles from './navbar.module.css';
+
+export function Navbar() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Navbar!</h1>
+    </div>
+  );
+}
+
+export default Navbar;
