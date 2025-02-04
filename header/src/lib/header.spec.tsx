e24eac0150e40c1f83ad9c6c4549cd8c58c86e4f
rafa-elle <rafaelledalusong@gmai.com>
2025-02-04 12:52:00 +0100

    feat: edit header.tsx and style

--- header/src/lib/header.spec.tsx
+++ header/src/lib/header.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import Header from './header';
+import Header from './Header';
 
 describe('Header', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Header />);
+    const { baseElement } = render(<Header title="Test Title" />);
     expect(baseElement).toBeTruthy();
   });
 });
