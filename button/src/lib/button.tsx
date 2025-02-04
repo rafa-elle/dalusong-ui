07c37bc543f951283a60f70009921a625d840f12
rafa-elle <rafaelledalusong@gmai.com>
2025-02-04 11:54:53 +0100

    feat: fix problem build

--- button/src/lib/button.tsx
+++ button/src/lib/button.tsx
@@ -1,13 +1,12 @@
-import { on } from 'events';
 import styles from './button.module.css';
 
 export function Button() {
   return (
-    <a href='https://www.google.com'>
-    <button className={styles.button}>
-      Click me
-    </button>
-    </a>
+    <>
+      <a href="https://www.google.com">
+        <button className={styles.button}>Click me</button>
+      </a>
+    </>
   );
 }
 
