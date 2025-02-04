dd3b89391601cf7106d42e0c17d35eda4a4eec85
rafa-elle <rafaelledalusong@gmai.com>
2025-02-04 14:41:07 +0100

    feat: add hero

--- hero/src/lib/hero.tsx
+++ hero/src/lib/hero.tsx
@@ -1,11 +1,24 @@
+// Hero.tsx
+import React from 'react';
 import styles from './hero.module.css';
 
-export function Hero() {
+const Hero = () => {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Hero!</h1>
-    </div>
+    <section className={styles.hero}>
+      {/* Titolo */}
+      <h1 className={styles.heroTitle}>Library</h1>
+
+      {/* Descrizione */}
+      <p className={styles.heroDescription}>
+        Library with NX console
+      </p>
+
+      {/* Pulsante */}
+      <button className={styles.heroButton} onClick={() => alert('Button clicked!')}>
+        Get Started
+      </button>
+    </section>
   );
-}
+};
 
 export default Hero;
